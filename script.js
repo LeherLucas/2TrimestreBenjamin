30ba76a06f32dcd7b8577a99dce3b88c79b97706
Lucas <127355311+LeherLucas@users.noreply.github.com>
2024-08-20 16:24:45 -0300

    digitar a história

--- script.js
+++ script.js
@@ -6,10 +6,31 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
         alternativa:[
-            "Alternativa 1",
-            "Alternativa 2"
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre IA. No fim de uma aula ela pede que Gabriel escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude Gabriel toma?",
+        alternativa:[
+            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
+        ]
+    },
+    {
+        enunciado: "Depois que Gabriel escreveu o trabalho, teve uma discussão sobre o impacto da IA no trabalho do futuro o que Gabriel faz:",
+        alternativa:[
+            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas",
+            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, Gabriel precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativa:[
+            "Criar uma imagem utilizando um gerador de imagem de IA.",
+            "Criar uma imagem utilizando uma plataforma de design como o Paint."
         ]
     },
     {
@@ -18,5 +39,13 @@ const perguntas = [
             "Alternativa 1",
             "Alternativa 2"
         ]
-    }
+    },
 ];
+
+let atual = 0; 
+let perguntaAtual; 
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
